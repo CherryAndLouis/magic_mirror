11757001b976b0b57823249fe315ffd7a9839f8f
CherryAndLouis <liu_xinyulive@163.com>
2022-05-10 14:30:18 +0800

    update

---
+++ result/2022-05-09 16-36-11.tcl
@@ -0,0 +1,30 @@
+<TESTCASE_BEGIN>
+<TESTCASE_HEADER_BEGIN>
+    <TITLE>      "执行用例自动化生成脚本"
+    <INDEX>      "执行编号"
+    <LEVEL>      "2"
+    <WEIGHT>     "4"
+    <MODULE>     "no_modename"
+    <TYPE>       "FUN"
+    <AUTHOR>     "Automatic Generation"
+    <LIMITATION> "CmwV7Device"
+    <VERSION> "2.1"
+    <DESIGN> "log自动化脚本"
+    <SOURCE> "isis.topo"
+<TESTCASE_HEADER_END>
+
+<TESTCASE_DEVICE_MAP_BEGIN>
+
+<TESTCASE_DEVICE_MAP_END>
+
+<STEP> "step 1" {
+  DUT1 Config "
+
+      reset logbuffer
+      sy
+      interface GigabitEthernet 2/0/1
+      interface GigabitEthernet2/0/1
+      qu
+      sy	"
+}
+<TESTCASE_END>
